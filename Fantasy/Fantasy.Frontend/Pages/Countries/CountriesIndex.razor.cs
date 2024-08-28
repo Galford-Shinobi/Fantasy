ba0fc9a82759147aaa4235226eebbce82fc03d89
danhernandezc <dhernandezc@desarrollo-ultrasist.com.mx>
2024-08-28 15:15:07 -0600

    Front End
    trabajando con el front para que tenga una mejor presentación

--- Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -1,6 +1,41 @@
-﻿namespace Fantasy.Frontend.Pages.Countries
+using CurrieTechnologies.Razor.SweetAlert2;
+using Fantasy.Frontend.Repositories;
+using Fantasy.Shared.Entites;
+using Fantasy.Shared.Resources;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace Fantasy.Frontend.Pages.Countries
 {
     public partial class CountriesIndex
     {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+        //[Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        private List<Country>? Countries { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadAsync();
+        }
+
+        private async Task LoadAsync()
+        {
+            var responseHppt = await Repository.GetAsync<List<Country>>("api/countries");
+            if (responseHppt.Error)
+            {
+                var message = await responseHppt.GetErrorMessageAsync();
+                //await SweetAlertService.FireAsync(Localizer["Error"], message, SweetAlertIcon.Error);
+                return;
+            }
+            Countries = responseHppt.Response!;
+        }
+
+        private async Task DeleteAsync(Country country)
+        {
+            await LoadAsync();
+        }
     }
 }
--- Fantasy/Fantasy.Frontend/Program.cs
+++ Fantasy/Fantasy.Frontend/Program.cs
@@ -7,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7252/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7278/") });
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddLocalization();
 await builder.Build().RunAsync();
