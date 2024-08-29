67f603efef99dd9f76139be61e5a5c9e1daabb1b
danhernandezc <dhernandezc@desarrollo-ultrasist.com.mx>
2024-08-28 18:13:47 -0600

    FrontEnd Modific
    Modificando el Front End

--- Fantasy/Fantasy.Frontend/Program.cs
+++ Fantasy/Fantasy.Frontend/Program.cs
@@ -1,7 +1,9 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Fantasy.Frontend;
 using Fantasy.Frontend.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,4 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7278/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddLocalization();
+builder.Services.AddSweetAlert2();
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
